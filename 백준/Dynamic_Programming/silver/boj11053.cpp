5cb55b76b16719552c1939596322f51670ea2d9e
Jae_Hoon(재훈) <astroy307@gmail.com>
2025-07-05 23:25:42 +0900

    Create boj11053.cpp

---
+++ 백준/Dynamic_Programming/silver/boj11053.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N; cin >> N;
+	vector<int>v(N+1,0);
+	vector<int>dp(N + 1, 0);
+	for (int i = 1; i <= N; ++i) {
+		int a; cin >> a;
+		v[i] = a;
+	}// vector setting
+
+	for (int i = 1; i <= N; ++i) {
+		int biggest = 0;
+		for (int j = i - 1; j >= 0; --j) {			
+			if (v[i] > v[j] && dp[j]>biggest) {
+				biggest = dp[j];
+			}
+		}
+		dp[i] = biggest + 1;
+	}
+
+	int big = 0;
+	for (int i = 0; i <= N; ++i) {
+		if (dp[i] > big) big = dp[i];
+	}
+
+	cout << big;
+}
